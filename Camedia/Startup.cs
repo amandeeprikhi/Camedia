97aa0c9acbe50e81da8033a9b90d38cc24d59c4a
Amandeep Rikhi <amandeeprikhi@gmail.com>
2020-03-26 03:06:56 +0530

    Add project files.

---
+++ Camedia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Camedia.Startup))]
+namespace Camedia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
